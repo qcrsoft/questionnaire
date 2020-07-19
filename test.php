7162554dbaf43531208b639bfea207bad9ecab9b
qcrsoft <qcrsoft@163.com>
2020-07-19 17:22:24 +1200

    test 4
    tttest

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+$age = array("Peter"=>35, "Ben"=>37, "Joe"=>43);
+
+echo json_encode($age);
+
+$conn=mysql_connect("localhost", "wa3239_1", "Ww040506#");
+$conn=mysql_connect("localhost", "wa3239_1", "Ww040506#");             
+
+?>
+ok
